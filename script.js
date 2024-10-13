9f0b33c9e5930ea7f21576b1bd74954f5bc008da
João Victor Simião Da Silva <54046448+joao9686@users.noreply.github.com>
2024-10-13 14:19:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 function abrirModal(modalId) {
-    document.getElementById(modalId).style.display = 'block';
-  }
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+  setTimeout(() => modal.classList.add('show'), 10);
+}
+
   
   function fecharModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
   }
+  document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+      var modals = document.getElementsByClassName('modal');
+      for (let modal of modals) {
+        modal.style.display = 'none';
+        modal.classList.remove('show');
+      }
+    }
+  });
+  
   
   window.onclick = function(event) {
     var modals = document.getElementsByClassName('modal');
