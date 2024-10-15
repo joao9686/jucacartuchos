10a4c4b016562a5af346fd88485d7d2462a1e74b
João Victor Simião Da Silva <54046448+joao9686@users.noreply.github.com>
2024-10-15 12:24:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,27 @@ function abrirModal(modalId) {
     }
   }
   
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do formulário
+
+    // Coletar os dados do formulário
+    const formData = new FormData(this);
+
+    // Enviar os dados via fetch API
+    fetch("https://formspree.io/f/mgveezer", {
+        method: "POST",
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function(response) {
+        if (response.ok) {
+            document.getElementById('formMessage').innerHTML = "Mensagem enviada com sucesso!";
+            document.getElementById('contactForm').reset(); // Limpa o formulário após o envio
+        } else {
+            document.getElementById('formMessage').innerHTML = "Ocorreu um erro. Tente novamente.";
+        }
+    }).catch(function(error) {
+        document.getElementById('formMessage').innerHTML = "Ocorreu um erro ao enviar o formulário.";
+    });
+});
